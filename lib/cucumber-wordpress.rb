99828af04c07955c02ff7be448dcf32b0a684b7c
Tom Adams <tom@holizz.com>
2009-11-19 12:22:38 +0000

    Make the generated config file smaller.

--- lib/cucumber-wordpress.rb
+++ lib/cucumber-wordpress.rb
@@ -48,12 +48,7 @@ define('DB_PASSWORD', '#{@DB_PASSWORD}');
 define('DB_HOST', '#{@DB_HOST}');
 define('DB_CHARSET', '#{@DB_CHARSET}');
 define('DB_COLLATE', '#{@DB_COLLATE}');
-define('AUTH_KEY', 'put your unique phrase here');
-define('SECURE_AUTH_KEY', 'put your unique phrase here');
-define('LOGGED_IN_KEY', 'put your unique phrase here');
-define('NONCE_KEY', 'put your unique phrase here');
 $table_prefix  = '#{@TABLE_PREFIX}';
-define ('WPLANG', '');
 if ( !defined('ABSPATH') ) define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'wp-settings.php');
 HERE
